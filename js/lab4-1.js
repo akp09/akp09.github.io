02c10a50058f4835d27ed415b4c56df4a450b279
akp09 <akp09@byui.edu>
2019-05-18 09:36:22 -0400

    ihh

--- js/lab4-1.js
+++ js/lab4-1.js
@@ -18,19 +18,3 @@ function myName() {
     //Output
     document.getElementById('output').innerHTML = ps;
 }
-/* Defining Table 4-2
-Input: get city as c, state as s, zip code as z 
-Process: put address in form c,s z as a
-Output: print result as output
-*/
-
-function addressBuilder() {
-//Input
-let c = document.getElementById("city").value;
-let s = document.getElementById("state").value;
-let z = document.getElementById("zip").value;
-//Process
-let a = c + ', ' + s + " " + z;
-//Output
-document.getElementById('output').innerHTML = a;
-}
